090fff6fc2f39f31fde98aeea52813eca2ec5750
Petar Shatanovski <petar_rk@yahoo.com>
2021-03-08 15:50:36 +0100

    initial

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Structura.Data;
+using Structura.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PetarSkinet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+        public ProductsController(StoreContext storeContext)
+        {
+            context = storeContext;
+        }
+        [HttpGet]
+        public async Task< ActionResult<List<Product>>> GetProducts()
+        {
+            var products =await context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task< ActionResult<Product>> GetProduct(int id)
+        {
+            var product =await context.Products.FindAsync(id);
+            return Ok(product);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Structura.Data;
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Structura
+{
+    public class Startup
+    {
+        private readonly IConfiguration conf;
+        public Startup(IConfiguration configuration)
+        {
+            conf = configuration;
+        }
+
+       
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddDbContext<StoreContext>(x=>x.UseSqlServer(conf.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PetarSkinet", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PetarSkinet v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
