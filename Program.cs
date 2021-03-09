f169280e98071b9d8d94e0a64bdd59a7208988f2
Petar Shatanovski <petar_rk@yahoo.com>
2021-03-09 12:43:22 +0100

    logger

--- Program.cs
+++ Program.cs
@@ -1,19 +1,40 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Structura.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Structura
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host=CreateHostBuilder(args).Build();
+            using(var scope=host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactoy = services.GetRequiredService<ILoggerFactory>();
+
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                }
+                catch(Exception ex)
+                {
+                    var logger = loggerFactoy.CreateLogger<Program>();
+                    logger.LogError(ex,"Greska pri migracija");
+                }
+            }
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
