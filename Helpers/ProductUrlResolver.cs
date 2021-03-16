68cafdd1d22af0fb6a439bf05afe789dfeca0f71
PetarSh <74772887+PetarSh@users.noreply.github.com>
2021-03-16 13:10:03 +0100

    auto mapper url

---
+++ Helpers/ProductUrlResolver.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using core1.Entities;
+using Microsoft.Extensions.Configuration;
+using PetarSkinet.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetarSkinet.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration configuration
+        public ProductUrlResolver(IConfiguration conf)
+        {
+            configuration = conf;
+        }
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
