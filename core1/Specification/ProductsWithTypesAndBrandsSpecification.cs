a25fd0bf0e31707816155b4a0d85b022d30e26f8
PetarSh <74772887+PetarSh@users.noreply.github.com>
2021-03-18 15:50:51 +0100

    sort price asc desc

--- core1/Specification/ProductsWithTypesAndBrandsSpecification.cs
+++ core1/Specification/ProductsWithTypesAndBrandsSpecification.cs
@@ -15,11 +15,27 @@ namespace core1.Specification
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
             AddOrderBy(x => x.Name);
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
-        public ProductsWithTypesAndBrandsSpecification(int id):base(x =>x.Id==id)
+            public ProductsWithTypesAndBrandsSpecification(int id):base(x =>x.Id==id)
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
         }
+        }
     }
-}
