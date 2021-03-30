a0633981a49c8a90ebc8000342f2cc431c01c7f9
PetarSh <74772887+PetarSh@users.noreply.github.com>
2021-03-30 11:47:23 +0200

    new Client project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,29 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  
-  title = 'PetarSkinet';
-
-  constructor(private http:HttpClient) { }
-  ngOnInit(): void {
-
-    this.http.get('https://localhost:44310/api/Products').subscribe((response: any) => {
-
-      console.log(response);
-
-    }, error => {
-      console.log(error);
-    }
-
-  );
-  }
-
-
+export class AppComponent {
+  title = 'Client';
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { from } from 'rxjs';
 
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
